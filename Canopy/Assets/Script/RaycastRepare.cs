e2a61ee0c1c13bb24728d7b4a655cab694ba883e
Jorrizo <jorris.hentz@gmail.com>
2020-02-04 09:07:34 +0100

    commencement du setup de l'IA, elle se déplace d'un point à un autre mais après elle a la flemme

---
+++ Canopy/Assets/Script/RaycastRepare.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastRepare : MonoBehaviour
+{
+
+    RaycastHit ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Physics.Raycast(transform.position, transform.forward, out ray, 5))
+        {
+            Debug.Log("yolo");
+        }
+    }
+}
