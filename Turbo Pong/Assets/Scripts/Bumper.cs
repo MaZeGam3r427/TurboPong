a18e828e9cc387856e339b42955e9ea99c07dd58
MaZeGam3r427 <samuelarsac@hotmail.com>
2020-02-19 09:39:45 +0100

    Pong fonctionnel (Score, But, Déplacement)

---
+++ Turbo Pong/Assets/Scripts/Bumper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public bool isBump1 = true;
+    public float speed = 5f;
+    public Rigidbody myRigid;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isBump1)
+        {
+            float MoveP1 = Input.GetAxis("VerticalP1");
+            Debug.Log(MoveP1);
+            if (MoveP1 != 0)
+            {
+                Vector3 movementP1 = new Vector3(0, MoveP1, 0);
+
+                myRigid.velocity = movementP1 * speed * Time.deltaTime;
+            }
+            
+        }
+        else
+        {
+            float MoveP2 = Input.GetAxis("VerticalP2");
+            Debug.Log(MoveP2);
+            if (MoveP2 != 0)
+            {
+                Vector3 movementP2 = new Vector3(0, MoveP2, 0);
+
+                myRigid.velocity = movementP2 * speed * Time.deltaTime;
+            }
+
+        }
+    }
+}
---
+++ Turbo Pong/Assets/Scripts/CheckGoal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckGoal : MonoBehaviour
+{
+    public GameObject Ball;
+    public TextMeshProUGUI ScoreP1;
+    public TextMeshProUGUI ScoreP2;
+    int var_ScoreP1 = 0;
+    int var_ScoreP2 = 0;
+    public Vector3 BallSP;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreP1.text = var_ScoreP1.ToString();
+        ScoreP2.text = var_ScoreP2.ToString();
+
+        BallSP = Ball.transform.position;
+
+        GameObject.Find("Ball").GetComponent<Ball>().Spawn();
+        
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.tag == "P1")
+        {
+            var_ScoreP1 += 1;
+            ScoreP1.text = var_ScoreP1.ToString();
+            Ball.transform.position = BallSP;
+        }
+
+        if (gameObject.tag == "P2")
+        {
+            var_ScoreP2 += 1;
+            ScoreP2.text = var_ScoreP2.ToString();
+            Ball.transform.position = BallSP;
+        }
+    }
+}
