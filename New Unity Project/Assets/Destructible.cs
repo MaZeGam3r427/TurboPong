383a19c4f496cb8ca6cc956b7d2376724e8a47bf
joseph pelloquet <joseph.pelloquet@gmail.com>
2020-03-04 09:13:33 +0100

    validation

---
+++ New Unity Project/Assets/Destructible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class Destructible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseUp()
+    {
+        savemanager.s_singleton.OnDestructibleDestroyed();
+        Destroy(gameObject);
+    }
+}
---
+++ New Unity Project/Assets/savemanager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class savemanager : MonoBehaviour
+{
+    private int nbDestroyed = 0;
+    public Transform TargetsParent;
+
+    public static savemanager s_singleton;
+
+    private void Awake()
+    {
+        if(s_singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_singleton = this;
+        }
+        //PlayerPrefs.DeleteAll();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        int tmpDestr = PlayerPrefs.GetInt("Detruits");
+        for (int i = 0; i < tmpDestr; i++)
+        {
+            Destroy(TargetsParent.GetChild(i).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDestructibleDestroyed ()
+    {
+        nbDestroyed++;
+        PlayerPrefs.SetInt("Detruits", nbDestroyed);
+        Debug.Log(nbDestroyed);
+    }
+}
