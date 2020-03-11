09873d1ba5db13610d3063af10639939f7c1841e
MaZeGam3r427 <samuelarsac@hotmail.com>
2020-03-11 22:41:07 +0100

    Transitions Menu principal & Choix des maps

---
+++ Turbo Pong/Assets/Scripts/LoaderLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderLevel : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public void TransitionMapMenu()
+    {
+        StartCoroutine(MapMenu(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public IEnumerator MapMenu (int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}   
+
