25e7ff80be833ec98bd5f76521126c1cd5c09cb9
Jorrizo <jorris.hentz@gmail.com>
2020-03-25 09:48:13 +0100

    lebonbug

---
+++ Turbo Pong/Assets/Scripts/GrowUpPowerup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowUpPowerup : MonoBehaviour
+{
+
+    public GameObject tallBar;
+    public GameObject normalBar;
+    private Vector3 positionNormalBar;
+    private Quaternion rotationNormalBar;
+
+    private float timer = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionNormalBar = normalBar.transform.position;
+        rotationNormalBar = normalBar.transform.rotation;
+
+        if (Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            Instantiate(tallBar, positionNormalBar, rotationNormalBar);
+            Destroy(normalBar);
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
